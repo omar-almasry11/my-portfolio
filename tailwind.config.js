d33ebf767ec001043d37db83b9ae501f924441f6
omar-almasry11 <hello@omaralmasry.com>
2024-12-24 20:40:07 +0200

    Added custom color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,liquid}"],
+  content: ["./src/**/*.{html,js,liquid}"], // Adjust paths if needed
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Inter', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#3d60e2', // Default primary color
+          light: '#e0ecff',   // Light variant
+        },
+        text: {
+          DEFAULT: '#080708', // Default text color
+          muted: '#707179',   // Muted text color
+        },
       },
     },
   },
