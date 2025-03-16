904b04fa2c457dd7f57e0048cf6e3dcc30cc28ac
omar-almasry11 <hello@omaralmasry.com>
2025-03-16 06:22:02 +0200

    updated font and stylistic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Atkinson', 'Inter', 'sans-serif'],
       },
       colors: {
         transparent: 'transparent',
